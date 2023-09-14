68c62cb97000b049548229652f2f00d16ed48982
juangtzi <90491496+juangtzi@users.noreply.github.com>
2023-09-14 16:28:50 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,53 +7,49 @@ import Joystick from '../components/Joystick';
 const Home = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-
   const roomWidth = 400;
   const roomHeight = 400;
   const robotSize = 10;
   const robotSpeed = 1;
 
-
   const handleMoveRobot = (direction) => {
     // Copia la posición actual para modificarla
     let { x, y } = position;
-  
+
     // Calcula la nueva posición en función de la dirección
     switch (direction) {
-      case 'FORWARD':
+      case "FORWARD":
         y = Math.max(0, y - robotSpeed);
         break;
-      case 'BACKWARD':
+      case "BACKWARD":
         y = Math.min(roomHeight - robotSize, y + robotSpeed);
         break;
-      case 'LEFT':
+      case "LEFT":
         x = Math.max(0, x - robotSpeed);
         break;
-      case 'RIGHT':
+      case "RIGHT":
         x = Math.min(roomWidth - robotSize, x + robotSpeed);
         break;
       default:
         break;
     }
-  
+
     // Actualiza la posición del robot
     setPosition({ x, y });
-    console.log(position)
-  
+    console.log(position);
+
     // Enviar la nueva posición (x, y) al servidor
   };
 
   return (
     <div>
-
-        <Room />
-        <div className='information'>
-          <p>X = {position.x}</p>
-          <p>Y = {position.y}</p>
-        </div>
-        <Robot x={position.x} y={position.y} />
-        <Joystick onMove={handleMoveRobot} />
-
+      <Room />
+      <div className="information">
+        <p>X = {position.x}</p>
+        <p>Y = {position.y}</p>
+      </div>
+      <Robot x={position.x} y={position.y} />
+      <Joystick onMove={handleMoveRobot} />
     </div>
   );
 };
